61ae0f03807033fe4e806e4fc1365d01e5f75887
Sandish12 <sandish.dangol@gmail.com>
2022-12-25 10:59:17 +0545

    2nd

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import ExpenseItem from './Components/ExpenseItem';
+import React from 'react';
+import Expenses from './Components/Expenses/Expenses';
+import NewExpense from './Components/NewExpense/NewExpense';
 
 function App() {
   const expenses = [
@@ -8,12 +10,7 @@ function App() {
       amount: 94.12,
       date: new Date(2020, 7, 14),
     },
-    { 
-      id: 'e2', 
-      title: 'New TV', 
-      amount: 799.49, 
-      date: new Date(2021, 2, 12) 
-    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
     {
       id: 'e3',
       title: 'Car Insurance',
@@ -27,31 +24,17 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
+const addExpenseHandler=expense=>{
+  console.log('In App.js')
+  console.log(expense)
+}
 
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      />
+      <NewExpense onAddExpense={addExpenseHandler}/>
+      <Expenses items={expenses} />
     </div>
   );
 }
+
 export default App;
